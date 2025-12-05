93ad8b63c7f98da9c59d278a7327538a137390cc
Milind <milind@askstatement.com>
2025-12-06 03:53:06 +0530

    refactor: add landing page improvements

--- frontend/src/app/(main)/layout.js
+++ frontend/src/app/(main)/layout.js
@@ -1,13 +1,11 @@
 'use client'
 import "@/style/layout/external.scss"
 import "@/style/globals.scss";
-import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
 export default function RootLayout({ children }) {
   return (
     <div className="external_layout">
-        <Header />
         <div className="page_content">{children}</div>
         <Footer />
     </div>
