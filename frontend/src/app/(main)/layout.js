b8656387ed76feabaccd2abbef63cbf6d6116a93
Milind <milind@askstatement.com>
2025-12-06 04:56:47 +0530

    refactor: add landing page font and remove layout

--- frontend/src/app/(main)/layout.js
+++ frontend/src/app/(main)/layout.js
@@ -1,13 +1,11 @@
 'use client'
 import "@/style/layout/external.scss"
 import "@/style/globals.scss";
-import Footer from "@/components/layout/footer";
 
 export default function RootLayout({ children }) {
   return (
     <div className="external_layout">
         <div className="page_content">{children}</div>
-        <Footer />
     </div>
   );
 }
